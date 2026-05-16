fd750d3c3120832ccca216c2ac2089f25ea764dc
Sebastián Cardona Henao <schsebastiancardonahenao@gmail.com>
2026-05-16 20:45:51 +0200

    feat(feature-stats): expand StatsError tree (Session, Read) + add androidHostTest + datetime dep

--- feature/stats/src/commonMain/kotlin/es/schsebastian/foodrats/feature/stats/domain/error/StatsError.kt
+++ feature/stats/src/commonMain/kotlin/es/schsebastian/foodrats/feature/stats/domain/error/StatsError.kt
@@ -1,8 +1,13 @@
 package es.schsebastian.foodrats.feature.stats.domain.error
 
 sealed interface StatsError {
-    sealed interface Aggregation : StatsError {
-        data object Unavailable : Aggregation
-        data object NotEnoughData : Aggregation
+    sealed interface Session : StatsError {
+        data object NoActiveCrew : Session
+        data object NotSignedIn : Session
+    }
+    sealed interface Read : StatsError {
+        data object Unauthorized : Read
+        data object CrewNotFound : Read
+        data object Unavailable : Read
     }
 }
