d9e45647bb95cb8fb9db28405259ae606e8b7694
Sebastián Cardona Henao <schsebastiancardonahenao@gmail.com>
2026-05-16 21:24:57 +0200

    feat(iosApp): AppDelegate + iOSApp wire Firebase + APNs + GoogleSignIn + UN delegate

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,26 +1,11 @@
 import SwiftUI
-import FoodRatsShared
 
 @main
 struct iOSApp: App {
-    init() {
-        // Firebase + GoogleSignIn require SPM packages added per SETUP.md.
-        // Once SPM is configured, uncomment the imports and these calls:
-        //   FirebaseApp.configure()
-        // and the .onOpenURL handler below.
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ComposeViewController()
-                .ignoresSafeArea(.keyboard)
-                // .onOpenURL { url in _ = GIDSignIn.sharedInstance.handle(url) }
+            ContentView()
         }
     }
-}
-
-struct ComposeViewController: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
-    }
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
