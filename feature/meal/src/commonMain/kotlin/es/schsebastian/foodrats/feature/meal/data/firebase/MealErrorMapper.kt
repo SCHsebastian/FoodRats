b6381e67417ff40c0bc973ce3c7e4a7cdd3a87b6
Sebastián Cardona Henao <schsebastiancardonahenao@gmail.com>
2026-05-19 16:36:07 +0200

    feat(feature-meal): MealErrorMapper.mapRate maps Firestore failures to RateError

--- feature/meal/src/commonMain/kotlin/es/schsebastian/foodrats/feature/meal/data/firebase/MealErrorMapper.kt
+++ feature/meal/src/commonMain/kotlin/es/schsebastian/foodrats/feature/meal/data/firebase/MealErrorMapper.kt
@@ -1,6 +1,7 @@
 package es.schsebastian.foodrats.feature.meal.data.firebase
 
 import es.schsebastian.foodrats.core.domain.meal.MealReadError
+import es.schsebastian.foodrats.core.domain.meal.RateError
 import es.schsebastian.foodrats.core.domain.telemetry.CrashReporter
 import es.schsebastian.foodrats.feature.meal.domain.error.MealError
 
@@ -22,5 +23,18 @@ class MealErrorMapper(private val crashReporter: CrashReporter) {
     fun mapRead(t: Throwable): MealReadError {
         crashReporter.recordNonFatal(t, tag = "meal-read")
         return MealReadError.Unavailable
+    }
+
+    fun mapRate(t: Throwable): RateError {
+        val message = t.message.orEmpty().lowercase()
+        return when {
+            "already-exists" in message || "already_exists" in message -> RateError.AlreadyRated
+            "permission" in message || "permission_denied" in message -> RateError.RateUnavailable
+            "unavailable" in message || "network" in message           -> RateError.RateUnavailable
+            else -> {
+                crashReporter.recordNonFatal(t, tag = "meal-rate-unmapped")
+                RateError.RateUnavailable
+            }
+        }
     }
 }
