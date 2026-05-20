1568c0676fea04a1028c8207387bd89df0588a36
Sebas <schsebastiancardonahenao@gmail.com>
2026-05-20 23:21:29 +0200

    refactor(domain): collapse MealComment.author to authorId; remove CommentAuthor
    Build will be red until Tasks 4 + 11 land; ships together on the feature branch.

--- core/domain/src/commonMain/kotlin/es/schsebastian/foodrats/core/domain/meal/MealComment.kt
+++ core/domain/src/commonMain/kotlin/es/schsebastian/foodrats/core/domain/meal/MealComment.kt
@@ -8,17 +8,11 @@ import kotlin.time.Instant
 @JvmInline
 value class MealCommentId(val value: String)
 
-data class CommentAuthor(
-    val accountId: AccountId,
-    val displayName: String,
-    val avatarUrl: String?,
-)
-
 data class MealComment(
     val id: MealCommentId,
     val mealId: MealId,
     val crewId: CrewId,
-    val author: CommentAuthor,
+    val authorId: AccountId,
     val text: CommentText,
     val createdAt: Instant,
 )
